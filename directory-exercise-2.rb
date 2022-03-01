699812a9271b3d1fa771f9d074e5f41b5fc8f2c4
Khushnood Asif <khushnood99@gmail.com>
2022-03-01 20:23:35 +0000

    exercise 2

---
+++ directory-exercise-2.rb
@@ -0,0 +1,45 @@
+def input_students
+   puts "Please enter the names of the students"
+   puts "To finish, just hit return twice"
+
+   students = []
+
+   name = gets.chomp
+
+   while !name.empty? do 
+      students << {name: name, cohort: :november}
+      puts "Now we have #{students.count} students"
+      name = gets.chomp
+   end
+   students
+end
+
+def print_header
+   puts "The students of Villains Academy"
+   puts "-------------"
+end
+
+def print(students)
+   students.each.with_index(1) do |student, index|
+      puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"}
+   end
+end
+
+def print_specific_letter(students)
+   puts "Which letter would you like to see? "
+   letter = gets.chomp
+   students.each.with_index(1) do |student, index|
+      if student[:name].chars.first == letter
+         puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"}
+      end
+end
+
+def print_footer(students)
+   puts "Overall, we have #{students.count} great students"
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
+print_specific_letter(students)

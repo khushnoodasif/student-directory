fface7d760e7f924f21667c62ffd7b2ad248615a
Khushnood Asif <khushnood99@gmail.com>
2022-03-01 18:17:00 +0000

    refactoring

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+student_count = 11
+
 puts "The students of Villians Academy"
 puts "-------------"
 
@@ -13,6 +15,4 @@ puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 
-print "Overall, we have "
-print 11
-puts " great students"
+print "Overall, we have #{student_count} great students"

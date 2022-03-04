e9d02ba243d43690b2ee2b1c4d1f339dae647f97
Khushnood Asif <khushnood99@gmail.com>
2022-03-04 16:49:53 +0000

    exercise 3

---
+++ directory-exercise-3.rb
@@ -0,0 +1,45 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  students = []
+
+  name = gets.chomp
+
+  while !name.empty? do 
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_12_char(students)
+  puts "Students with names less than 12 characters"
+  students.each do |student|
+    if student[:name].length < 12
+      puts "#{student[:name]}"
+    end
+  end
+end  
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+students = input_students
+print_header
+print(students)
+print_12_char(students)
+print_footer(students)
